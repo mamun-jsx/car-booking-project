a3eda3eadc7ce115026d0f2093720d4f514a8203
almamun528 <ayapths@gmail.com>
2025-09-03 22:33:50 +0600

    install marque and add component

---
+++ client/src/component/Popular-Brand/PopularBrand.jsx
@@ -0,0 +1,14 @@
+import Title from "../Title";
+
+export default function PopularBrand() {
+  return (
+    <section className="max-w-7xl mx-auto">
+      <Title
+        title={"Popular Brands in Our Collection"}
+        subTitle={
+          "Explore a wide range of top car brands available for rent or purchase."
+        }
+      />
+    </section>
+  );
+}
