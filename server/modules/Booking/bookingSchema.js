81e643fd25a2cfde32367f3bb013aa08a03679c6
almamun528 <ayapths@gmail.com>
2025-09-04 19:19:01 +0600

    bookingSchema

---
+++ server/modules/Booking/bookingSchema.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema.Types;
+const bookingSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: String,
+      ref: "User",
+      required: true,
+    },
+    car: { type: ObjectId, ref: "Car", required: true },
+    user: { type: ObjectId, ref: "User", required: true },
+
+    // pickup date and return date
+    pickupDate: { type: Date, required: true },
+    returnDate: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "cancelled"],
+      default: "pending",
+    },
+    price: { type: Number, required: true },
+  },
+
+  { timestamps: true }
+);
+
+const Booking = mongoose.model("Car", bookingSchema);
+export default Booking;
